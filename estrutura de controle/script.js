52fc9116675de3bc57472c2b30f962e2a923556a
outlooknati <natisales2107@gmail.com>
2025-10-02 09:43:11 -0300

    Add files via upload

--- estrutura de controle/script.js
+++ estrutura de controle/script.js
@@ -1,4 +1,4 @@
-const idade = document.getElementById('idade');
+const idadeInput = document.getElementById('idadeInput');
 const verifyButton = document.getElementById('verifyButton');
 const resultado = document.getElementById('resultado');
 
@@ -16,4 +16,56 @@ function verificarIdade(){
   }else{
     menssagem = 'Você é idoso'
   }
+
+  setTimeout(() => {
+    resultado.innerText = menssagem;
+    resultado.classList.add('visivel');
+  }, 100);
+}
+
+verifyButton.addEventListener('click', verificarIdade);
+idadeInput.addEventListener('keyup', (event) => {
+    if(event.key === 'Enter') verificarIdade ();
+});
+
+const canvas = document.getElementById('background-canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: 150
+};
+
+window.addEventListener('mouse move', (event) => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+let particulasArray = [];
+const numeroDeParticulas = 100;
+
+class Particula{
+    constructor(x, y, direcaoX, direcaoY, tamanho, cor){
+        this.x = x;
+        this.y = y;
+        this.direcaoX = direcaoX;
+        this.direcaoY = direcaoY;
+        this.tamanho = tamanho;
+        this.cor =cor;
+    }
+
+    desenhar(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.tamanho, 0, math.PI * 2, false);
+        ctx.fillStyle = '#007bff';
+        ctx.fill();
+    }
+
+
+    
+
 }
